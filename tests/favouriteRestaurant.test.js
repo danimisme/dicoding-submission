bc9ef627e3858439adb15345be77af510d399572
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2024-12-06 00:10:00 +0700

    create add  favourite test

--- tests/favouriteRestaurant.test.js
+++ tests/favouriteRestaurant.test.js
@@ -23,4 +23,31 @@ describe('Add Restaurant Favourite', () => {
     expect(document.querySelector('[aria-label="favourite this restaurant"]')).toBeTruthy();
   });
 
+  it('should not show the unfavourite button when the restaurant has not been favourited before', async () => {
+    await FavouriteButtonInitiator.init({
+      favouriteButtonContainer: document.querySelector('#favouriteButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unfavourite this restaurant"]')).toBeFalsy();
+  });
+
+  it('should be able to add favourite restaurant', async () => {
+    await FavouriteButtonInitiator.init({
+      favouriteButtonContainer: document.querySelector('#favouriteButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('#favouriteButton').dispatchEvent(new Event('click'));
+
+    const restaurant = await FavouriteRestaurant.getRestaurant(1);
+    expect(restaurant).toEqual({ id: 1 });
+
+    await FavouriteRestaurant.deleteRestaurant(1);
+  });
+
 });
