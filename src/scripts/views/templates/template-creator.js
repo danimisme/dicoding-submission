7e6bb847b582e20519f90ec70a53b44d96d4b13c
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2024-12-06 00:45:58 +0700

    add lazyloading in image

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -2,7 +2,7 @@ import CONFIG from '../../globals/config';
 
 const createRestaurantDetailTemplate = (restaurant) => `
   <h2 class="restaurant__title">${restaurant.name}</h2>
-  <img class="restaurant__poster" src="${restaurant.pictureId ? `${CONFIG.BASE_IMAGE_URL}/medium/${restaurant.pictureId}` : 'https://picsum.photos/id/666/800/450?grayscale'}" alt="${restaurant.name}" />
+  <img loading="lazy" class="restaurant__poster" src="${restaurant.pictureId ? `${CONFIG.BASE_IMAGE_URL}/medium/${restaurant.pictureId}` : 'https://picsum.photos/id/666/800/450?grayscale'}" alt="${restaurant.name}" />
   <div class="restaurant__info">
   <h3>Information</h3>
     <h4>Category</h4>
@@ -63,7 +63,7 @@ const createRestaurantDetailTemplate = (restaurant) => `
 const createRestaurantItemTemplate = (restaurant) => `
   <div class="restaurant-item">
     <div class="restaurant-item__header">
-      <img class="restaurant-item__header__poster" alt="${restaurant.name}"
+      <img loading="lazy" class="restaurant-item__header__poster" alt="${restaurant.name}"
            src="${restaurant.pictureId ? `${CONFIG.BASE_IMAGE_URL}/medium/${restaurant.pictureId}` : 'https://picsum.photos/id/666/800/450?grayscale'}">
       <div class="restaurant-item__header__rating">
         <p>⭐️<span class="restaurant-item__header__rating__score">${restaurant.rating}</span></p>
