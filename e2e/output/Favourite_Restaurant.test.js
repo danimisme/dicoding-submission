e90609263e722ed40db427dce70260e484ac2832
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2024-12-09 22:37:56 +0700

    create e2e test

---
+++ e2e/output/Favourite_Restaurant.test.js
@@ -0,0 +1,10 @@
+Feature('Favourite Restaurant');
+
+Before(({ I }) => {
+  I.amOnPage('/#/favourite');
+})
+ 
+Scenario('test something', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada restoran untuk ditampilkan', '.restaurant-item__not__found');
+});
