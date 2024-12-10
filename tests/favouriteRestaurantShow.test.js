de1eb69d83edc1168f10029e3b442b571c0c7a4c
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2024-12-10 22:22:48 +0700

    fix typing

--- tests/favouriteRestaurantShow.test.js
+++ tests/favouriteRestaurantShow.test.js
@@ -1,12 +1,13 @@
-const { default: FavoriteRestaurantSearchView } = require('../src/scripts/views/pages/favourited-restaurant/favourite-restaurant-search-view');
-const { default: FavoriteRestaurantShowPresenter } = require('../src/scripts/views/pages/favourited-restaurant/favourite-restaurant-show-presenter');
+import FavoriteRestaurantSearchView from '../src/scripts/views/pages/favourited-restaurant/favourite-restaurant-search-view';
+import FavoriteRestaurantShowPresenter from '../src/scripts/views/pages/favourited-restaurant/favourite-restaurant-show-presenter';
+import { describe, it, expect, beforeEach, jest } from '@jest/globals';
 
 describe('Showing all favorite Restaurants', () => {
   let view;
 
   const renderTemplate = () => {
     view = new FavoriteRestaurantSearchView();
-    document.body.innerHTML = view.getFavoriteRestaurantTemplate();
+    document.body.innerHTML = view.getTemplate();
   };
   beforeEach(() => {
     renderTemplate();
@@ -39,8 +40,81 @@ describe('Showing all favorite Restaurants', () => {
       });
       expect(favoriteRestaurant.getAllRestaurants).toHaveBeenCalledTimes(1);
     });
+
+    it('should show the information that no Restaurants have been liked', (done) => {
+      document.getElementById('restaurants').addEventListener('restaurants:updated', () => {
+        expect(document.querySelectorAll('.restaurant-item__not__found').length).toEqual(1);
+        done();
+      });
+
+      const favoriteRestaurant = {
+        getAllRestaurants: jest.fn().mockImplementation(() => []),
+      };
+
+      new FavoriteRestaurantShowPresenter({
+        view,
+        favoriteRestaurant,
+      });
+    });
   });
 
 
+  describe('When favorite Restaurants exist', () => {
+    it('should render the Restaurants', () => {
+      const favoriteRestaurant = {
+        getAllRestaurants: jest.fn().mockImplementation(() => []),
+      };
+
+      const presenter = new FavoriteRestaurantShowPresenter({
+        view,
+        favoriteRestaurant,
+      });
+
+      presenter._displayRestaurants([
+        {
+          id: 'restaurant-1',
+          name: 'Restaurant 1',
+          description: 'Description 1',
+        },
+        {
+          id: 'restaurant-2',
+          name: 'Restaurant 2',
+          description: 'Description 2',
+        },
+      ]);
+
+      expect(document.querySelectorAll('.restaurant-item').length).toEqual(2);
+    });
+
+    it('should show the restaurants', (done) => {
+      document.getElementById('restaurants').addEventListener('restaurants:updated', () => {
+        expect(document.querySelectorAll('.restaurant-item').length).toEqual(2);
+        done();
+      });
+
+      const favoriteRestaurant = {
+        getAllRestaurants: jest.fn().mockImplementation(() => [
+          {
+            id: 'restaurant-1',
+            name: 'Restaurant 1',
+            description: 'Description 1',
+          },
+          {
+            id: 'restaurant-2',
+            name: 'Restaurant 2',
+            description: 'Description 2',
+          },
+        ]),
+      };
+
+      new FavoriteRestaurantShowPresenter({
+        view,
+        favoriteRestaurant,
+      });
+    });
+
+
+
+  });
 
 });
